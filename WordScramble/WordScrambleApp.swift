ea24de5b1573a40b5e8cea2eb59516d59f412fca
ryangprince <rpprince18@gmail.com>
2022-10-25 23:10:26 -0400

    first commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ryan Prince on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
